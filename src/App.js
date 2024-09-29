792c830acb4ae43a2741ee951457d37df29bc8ee
YanaNaumova <naumovayana87@gmail.com>
2024-09-29 22:44:21 +0200

    praktic-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+/**@jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useState } from "react";
+import {
+  ThemeProvider,
+  createTheme,
+  CssBaseline,
+  TextField,
+  Card,
+} from "@mui/material";
 
+const darkTheme = createTheme({
+  palette: { mode: "dark" },
+});
 function App() {
+  const [text, setText] = useState("");
+  const [showText, setShowText] = useState([]);
+  const MyButton = styled.button`
+    background-color: red;
+    color: white;
+    cursor: pointer;
+    border: none;
+    border-radius: 8px;
+    height: 30px;
+    padding: 5px;
+    width: max-content;
+  `;
+
+  const cardStyle = css`
+    opacity: 0.6;
+    color: white;
+  `;
+
+  const inputStyle = css`
+    padding: 5px;
+    width: max-content;
+    background-color: lila;
+  `;
+
+  const containerStyle = css`
+    display: flex;
+    align-items: center;
+  `;
+  function handelButton() {
+    setShowText([...showText, text]);
+    setText("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div css={containerStyle}>
+        <TextField
+          label="Enter text"
+          css={inputStyle}
+          variant="outlined"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          placeholder="Text from the field"
+        />
+        <MyButton onClick={handelButton}>Display Text</MyButton>
+      </div>
+      {showText.length === 0 ? (
+        <Card variant="outlined" css={cardStyle}>
+          Text from the field
+        </Card>
+      ) : (
+        showText.map((text) => {
+          return (
+            <Card variant="outlined" css={cardStyle}>
+              {text}
+            </Card>
+          );
+        })
+      )}
+    </ThemeProvider>
   );
 }
 
